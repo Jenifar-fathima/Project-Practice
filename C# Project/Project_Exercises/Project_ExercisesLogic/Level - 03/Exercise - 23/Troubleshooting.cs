3b7ffb7edbc6f6a2d555a0561f9ac386f6f28a7c
JeniferFathimaH <jeniferfathima2103@gmail.com>
2024-05-23 17:23:32 +0530

    ADDED: Project_Exercise > Level - 03 > Exercise - 23

---
+++ C# Project/Project_Exercises/Project_ExercisesLogic/Level - 03/Exercise - 23/Troubleshooting.cs
@@ -0,0 +1,43 @@
+﻿namespace Project_ExercisesLogic
+{
+    public static class TroubleShooting
+    {
+        public static string TroubleshootIssue(bool isSilent, bool corrodedTerminals, bool clickingNoise, bool crankNoStart, bool engineStartDie, bool hasFuelInjection)
+        {
+            if (isSilent)
+            {
+                if (corrodedTerminals)
+                {
+                    return "Clean terminals and try starting again.";
+                }
+                else
+                {
+                    return "Replace cables and try again.";
+                }
+            }
+            else if (clickingNoise)
+            {
+                return "Replace the battery.";
+            }
+            else if (crankNoStart)
+            {
+                return "Check spark plug connections.";
+            }
+            else if (engineStartDie)
+            {
+                if (hasFuelInjection)
+                {
+                    return "Get it in for service.";
+                }
+                else
+                {
+                    return "Check to ensure the choke is opening and closing.";
+                }
+            }
+            else
+            {
+                return "Unknown issue.";
+            }
+        }
+    }
+}
